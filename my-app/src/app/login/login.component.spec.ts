806ec526ca0af10398c3910634674f2d10ad6be3
Rohit200 <rohitmohanpur7@gmail.com>
2018-11-27 17:30:08 +0530

    add existing file

--- my-app/src/app/login/login.component.spec.ts
+++ my-app/src/app/login/login.component.spec.ts
@@ -22,4 +22,26 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+it('should create', () => {
+  expect(1 + 1).toBeTruthy(2);
 });
+it('should create', () => {
+  expect(1 + 1).toBeTruthy(2);
+});
+
+  it('Form should be valid '), async(() => {
+  expect(component.loginForm.controls['email'].setValue('aishsovani1234@gmail.com'));
+  expect(component.loginForm.controls['password'].setValue('aishwarya'));
+  expect(component.loginForm.controls['email']).toBeTruthy();
+  expect(component.loginForm.controls['password']).toBeTruthy();
+  });
+
+  it('Invalid Form'), async(() => {
+  expect(component.loginForm.controls['email'].setValue(''));
+  expect(component.loginForm.controls['password'].setValue(''));
+  expect(component.loginForm.controls['email']).toBeFalsy();
+  expect(component.loginForm.controls['password']).toBeFalsy();
+  });
+});
+
