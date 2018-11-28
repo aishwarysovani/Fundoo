679837c616854dec41e3f22e48fcff2736661094
Rohit200 <rohitmohanpur7@gmail.com>
2018-11-28 18:19:56 +0530

    add existing file

--- my-app/src/app/note/notefilter.pipe.ts
+++ my-app/src/app/note/notefilter.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NotefilterPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    debugger;
+   
     if (!args) return value;
     return value.filter(items => {
     return (
-    items.Note.includes(args) == true || items.Title.includes(args) == true
+    items.note.includes(args) == true || items.title.includes(args) == true
     );
     });
     }
